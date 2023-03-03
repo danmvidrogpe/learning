d525ab5e23d45545c9962f12f4af9376b22d0df4
danmvidrogpe <daniel.vidal@rogpe.tech>
2023-03-03 06:26:47 -0300

    changed metod to .each

--- quant_a.rb
+++ quant_a.rb
@@ -4,7 +4,7 @@ phrase = gets.chomp
 count = 0
 phrase_array = phrase.split('')
 
-phrase_array.each_with_index do |char, position|
+phrase_array.each do |char|
 	if char == 'a' || char == 'A'
 		count = count + 1
 	end
